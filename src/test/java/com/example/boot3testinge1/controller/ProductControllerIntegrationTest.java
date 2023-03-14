14b1a4999ecfd70c829577f0da1c3e3a5edb4bfe
Jyotirmaya <jyotirmaya12@gmail.com>
2023-03-14 18:50:57 +0530

    Removed H2 database and updated ProductControllerIntegrationTest

--- src/test/java/com/example/boot3testinge1/controller/ProductControllerIntegrationTest.java
+++ src/test/java/com/example/boot3testinge1/controller/ProductControllerIntegrationTest.java
@@ -4,13 +4,19 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @AutoConfigureMockMvc
+@Testcontainers
+@TestPropertySource(properties = {
+        "spring.datasource.url=jdbc:tc:postgresql:15.2-alpine:///customers"
+})
 class ProductControllerIntegrationTest {
 
     @Autowired
