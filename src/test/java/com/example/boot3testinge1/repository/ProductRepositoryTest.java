1c943ccd2826cc60803e3e38d913ef41c2c478d4
Jyotirmaya <jyotirmaya12@gmail.com>
2023-03-14 22:04:56 +0530

    Configured to work with postgres container

--- src/test/java/com/example/boot3testinge1/repository/ProductRepositoryTest.java
+++ src/test/java/com/example/boot3testinge1/repository/ProductRepositoryTest.java
@@ -2,24 +2,22 @@ package com.example.boot3testinge1.repository;
 
 import com.example.boot3testinge1.model.Product;
 import jakarta.persistence.EntityManager;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.TestPropertySource;
-import org.testcontainers.junit.jupiter.Testcontainers;
 
 import java.math.BigDecimal;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-
 @DataJpaTest
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
-@Testcontainers
 @TestPropertySource(properties = {
-        "spring.datasource.url=jdbc:tc:postgresql:15.2-alpine:///customers"
+        "spring.datasource.url=jdbc:tc:postgresql:15.2-alpine:///products"
 })
 class ProductRepositoryTest {
 
@@ -28,6 +26,11 @@ class ProductRepositoryTest {
 
     @Autowired
     private EntityManager entityManager;
+
+    @BeforeEach
+    void setUp() {
+        productRepository.deleteAllInBatch();
+    }
 
     @Test
     void findAllActiveProducts() {
